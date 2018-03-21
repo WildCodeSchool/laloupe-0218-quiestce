1ae1dfa3e6ffd9090b9ae93cf9149ae1af33807d
BenjaminPerrin <perrin.benjamin77@gmail.com>
2018-03-21 15:21:10 +0100

    angularfire+bootstrap

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  constructor(db: AngularFirestore) {
+
+  }
+  title = 'bob';
+}
